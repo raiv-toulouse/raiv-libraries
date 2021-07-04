2637432240ad3060c084b30aeffcc669fd692103
raiv.toulouse@icam.fr <raiv.toulouse@icam.fr>
2021-07-04 17:43:12 +0200

    Add comments

--- src/raiv_libraries/image_viewer_opencv.py
+++ src/raiv_libraries/image_viewer_opencv.py
@@ -5,11 +5,17 @@ from sensor_msgs.msg import Image
 import cv2, cv_bridge
 from raiv_libraries.srv import InitDirectory,InitDirectoryResponse
 
+#
+# Subscribe to the image topic (default : /usb_cam/image_raw) and display its images.
+# You can save the current images with this service :
+# rosservice call /record_image "data: '/home/philippe'"   (where /home/philippe is the folder where the image is saved)
+#
+
 class ImageViewerOpenCV:
-  def __init__(self):
+  def __init__(self, image_topic='/usb_cam/image_raw'):
     self.indImage = 0
     self.bridge = cv_bridge.CvBridge()
-    self.image_sub = rospy.Subscriber('/usb_cam/image_raw',
+    self.image_sub = rospy.Subscriber(image_topic,
                                       Image, self.display_image)
     s = rospy.Service('record_image', InitDirectory, self.record_image)
     rospy.loginfo("Service record_image *******************************")
