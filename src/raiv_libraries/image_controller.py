1f66dd6a72beb3fbabdabb58ca91350f9878fc10
raiv-toulouse@icam.fr <raiv-toulouse>
2021-10-06 09:14:05 +0200

    SimpleImageController just to get an image from a topic and ImageController if you want to save it in a folder

--- src/raiv_libraries/image_controller.py
+++ src/raiv_libraries/image_controller.py
@@ -7,30 +7,28 @@ import time
 import rospy
 from PIL import Image as PILImage
 from sensor_msgs.msg import Image
+from raiv_libraries.simple_image_controller import SimpleImageController
 
 """
 This class is used to manage sensor_msgs Images.
 """
 
-class ImageController:
+class ImageController(SimpleImageController):
     def __init__(self, path=os.path.dirname(os.path.realpath(__file__)), image_topic='/usb_cam/image_raw'):
+        super().__init__(image_topic)
         self.ind_saved_images = 0  # Index which will tell us the number of images that have been saved
         self.success_path = "{}/success".format(path)  # Path where the images are going to be savedF
         self.fail_path = "{}/fail".format(path)  # Path where the images are going to be saved
-        self.image_topic = image_topic
+        self._create_if_not_exist(path)
         self._create_if_not_exist(self.success_path)
         self._create_if_not_exist(self.fail_path)
 
     def _create_if_not_exist(self, path):
         # If it does not exist, we create the path folder in our workspace
         try:
-            os.stat(self.path)
+            os.stat(path)
         except:
-            os.mkdir(self.path)
-
-    def get_image(self):
-        msg = rospy.wait_for_message(self.image_topic, Image)
-        return self.to_pil(msg), msg.width, msg.height
+            os.mkdir(path)
 
     def record_image(self, img, success):
         path = self.success_path if success else self.fail_path  # The path were we want to save the image is
@@ -42,14 +40,10 @@ class ImageController:
 
         self.ind_saved_images += 1  # Index increment
 
-    def to_pil(self, msg, display=False):
-        size = (msg.width, msg.height)  # Image size
-        img = PILImage.frombytes('RGB', size, msg.data)  # sensor_msg to Image
-        return img
 
 if __name__ == '__main__':
     rospy.init_node('image_recorder')  # ROS node initialization
-    image_controller = ImageController(path='/home/student1/ros_pictures', image_topic='/usb_cam/image_raw')
+    image_controller = ImageController(path='/home/phil/ros_pictures', image_topic='/usb_cam/image_raw')
     while True:
         img, width, height = image_controller.get_image()
         image_controller.record_image(img, True)
