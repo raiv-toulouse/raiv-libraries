ab825c9799a3430b34c311002d2b2c43cd423d37
Philippe Juhel <philippe.juhel@icam.fr>
2022-11-17 09:44:36 +0100

    Use InBoxCoord constants for crop size

--- src/raiv_libraries/test_get_coord_node.py
+++ src/raiv_libraries/test_get_coord_node.py
@@ -9,8 +9,6 @@ from raiv_libraries.srv import get_coordservice
 from raiv_libraries.get_coord_node import InBoxCoord
 
 
-CROP_WIDTH = CROP_HEIGHT = 50
-
 rospy.init_node('test_get_coord_node')
 rgb = rospy.wait_for_message("/camera/color/image_raw",  Image)
 rgb = CvBridge().imgmsg_to_cv2(rgb, desired_encoding ='bgr8')
@@ -23,9 +21,9 @@ coord_serv = rospy.ServiceProxy('In_box_coordService', get_coordservice)
 ct = 0
 while True:
     try:
-        resp = coord_serv('random', InBoxCoord.PICK, InBoxCoord.ON_OBJECT, CROP_WIDTH, CROP_HEIGHT, None, None)
+        resp = coord_serv('random', InBoxCoord.PICK, InBoxCoord.ON_OBJECT, InBoxCoord.CROP_WIDTH, InBoxCoord.CROP_HEIGHT, None, None)
         cv2.circle(rgb, (resp.x_pixel, resp.y_pixel), radius=2, color=(0, 0, 255), thickness=-1)
-        resp = coord_serv('random', InBoxCoord.PLACE, InBoxCoord.IN_THE_BOX, CROP_WIDTH, CROP_HEIGHT, None, None)
+        resp = coord_serv('random', InBoxCoord.PLACE, InBoxCoord.IN_THE_BOX, InBoxCoord.CROP_WIDTH, InBoxCoord.CROP_HEIGHT, None, None)
         cv2.circle(rgb, (resp.x_pixel, resp.y_pixel), radius=2, color=(255, 0, 0), thickness=-1)
         ct += 1
         print(ct)
